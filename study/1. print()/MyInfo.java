c165d3d381e932214adad350f3bee223d1910dab
i12free011@gmail.com <i12free011@gmail.com>
2023-08-29 16:39:52 +0900

    ~ 230828 split

---
+++ study/1. print()/MyInfo.java
@@ -0,0 +1,19 @@
+package Java01_출력;
+
+public class MyInfo {
+	public static void main(String[] args) {
+		
+		int age = 21;
+		char name1 = '모';
+		char name2 = '리';
+		
+		
+		System.out.print("이름: ");
+		System.out.println("" + name1 + name2);
+		System.out.print("나이: ");
+		System.out.println(age);
+		System.out.print("주소: ");
+		System.out.println("부산");
+		System.out.println();
+	}
+}
---
+++ study/1. print()/PrintTest.java
@@ -0,0 +1,10 @@
+package Java01_출력;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		System.out.println("HEllo, Java!");
+
+	}
+
+}
